2dbaf0dc51a4a378e13d18cea29e2936d1ea7cdc
Souravkamalkar8 <71004220+Souravkamalkar8@users.noreply.github.com>
2023-02-09 21:02:48 +0530

    Update AIFF.py

--- AIFF.py
+++ AIFF.py
@@ -125,7 +125,7 @@ with col6:
     st.metric('Clean sheets', value= df['Clean sheets'][j])
 
 #Third Row
-st.header(player + ' Scouting Report')
+st.title(player + ' Scouting Report')
 st.subheader('vs.'+' '+ df['Role'][j] + 's in ' + df['Competition'][j] + ' '+ df['Season'][j] )
 # Figure Size
 fig, ax = plt.subplots(figsize =(16, 9))
