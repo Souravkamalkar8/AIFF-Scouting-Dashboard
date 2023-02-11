977dc07f0195fe0b9581edf689c9c940a467bb4a
Souravkamalkar8 <71004220+Souravkamalkar8@users.noreply.github.com>
2023-02-11 21:03:29 +0530

    Update AIFF.py

--- AIFF.py
+++ AIFF.py
@@ -111,14 +111,19 @@ values= [round(df['Non-Pen Goals'][j],2),
          round(df['Aerial duels won'][j],2)]
 
 st.header('Detailed Stats')
-tab1, tab2 = st.tabs(["📈 Chart", "🗃 Data"])
-data = np.random.randn(10, 1)
+tab1, tab2, tab3 = st.tabs(["Cat", "Dog", "Owl"])
 
-tab1.subheader("A tab with a chart")
-tab1.line_chart(data)
+with tab1:
+   st.header("A cat")
+   st.image("https://static.streamlit.io/examples/cat.jpg", width=200)
 
-tab2.subheader("A tab with the data")
-tab2.write(data)
+with tab2:
+   st.header("A dog")
+   st.image("https://static.streamlit.io/examples/dog.jpg", width=200)
+
+with tab3:
+   st.header("An owl")
+   st.image("https://static.streamlit.io/examples/owl.jpg", width=200)
 
 #First Row 
 c1,c2= st.columns(2)
