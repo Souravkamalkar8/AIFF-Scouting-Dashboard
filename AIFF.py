0634fe9af778039366eb3d904eccc850d732ccde
Souravkamalkar8 <71004220+Souravkamalkar8@users.noreply.github.com>
2023-02-12 11:17:48 +0530

    Update AIFF.py

--- AIFF.py
+++ AIFF.py
@@ -22,7 +22,7 @@ width, height = img.size
 area = (0, 0, width, height/1.32)
 vision = img.crop(area)
 vision = vision.resize((200, 100))
-st.sidebar.image(vision,use_column_width = True)
+st.sidebar.image(vision,use_column_width ='auto')
 
 
 st.sidebar.header('Player Scouting Dashboard')
